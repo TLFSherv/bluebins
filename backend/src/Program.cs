d6e21d7da0d9ce48fc527ae37611865feeff536a
Sherwin Burgess <sherwinburgess@gmail.com>
2026-06-05 11:50:40 +0100

    Add database connection, remove default application css

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>( // register DbContext
+    options => options.UseNpgsql(connString) // specify provider
+);
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+var app = builder.Build();
+app.MapIdentityApi<ApplicationUser>();
+
+app.MapGet("/", () => "Hello World!")
+.RequireAuthorization();
 
 app.Run();
